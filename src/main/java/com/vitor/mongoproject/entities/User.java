6018d621c0f1d159ad9bb65539200f321fa705b9
mmartins-vitor <mmartins.vitor@hotmail.com>
2024-04-19 15:39:45 -0300

    conexao com mongo part. 1

--- src/main/java/com/vitor/mongoproject/entities/User.java
+++ src/main/java/com/vitor/mongoproject/entities/User.java
@@ -2,11 +2,16 @@ package com.vitor.mongoproject.entities;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable{
     private static final long serialVersionUID = 1L;
     // id
     // name
     // email
+    @Id
     public String id;
     public String name;
     public String email;
