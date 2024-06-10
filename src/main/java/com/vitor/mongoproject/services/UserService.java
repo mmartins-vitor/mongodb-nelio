13816e0f76c96b093ec9c07156c5460c3600b37a
mmartins-vitor <mmartins.vitor@hotmail.com>
2024-06-10 14:34:04 -0300

    inserção de user com post

--- src/main/java/com/vitor/mongoproject/services/UserService.java
+++ src/main/java/com/vitor/mongoproject/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vitor.mongoproject.dto.UserDTO;
 import com.vitor.mongoproject.entities.User;
 //import com.vitor.mongoproject.repository.UserRepository;
 import com.vitor.mongoproject.repository.UserRepository;
@@ -25,5 +26,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!!!"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
